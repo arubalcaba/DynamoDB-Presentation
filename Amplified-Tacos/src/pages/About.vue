f355b164f9d1b150247ee747b1cd10a612dd230c
Andrew Rubalcaba <arubalcaba@806technologies.com>
2024-10-17 20:40:19 -0500

    initial commit
    initial commit

---
+++ Amplified-Tacos/src/pages/About.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <h1>About Page</h1>
+      <p>Welcome to the About Page!</p>
+    </div>
+  </template>
+
+  <script lang="ts" setup>
+    //
+  </script>
---
+++ Amplified-Tacos/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home Page!</p>
+    </div>
+  </template>
+
+  <script lang="ts" setup>
+    //
+  </script>
